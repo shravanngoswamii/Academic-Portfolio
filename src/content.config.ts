45a638333413a055c5786108e1a9c895ffcb127a
Shravan Goswami <shravanngoswamii@gmail.com>
2026-01-29 01:56:27 +0530

    Add a favicon

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,11 +1,13 @@
 import { defineCollection, z } from 'astro:content';
 import { glob } from 'astro/loaders';
 
+type SchemaContext = { image: () => any };
+
 const blog = defineCollection({
 	// Load Markdown and MDX files in the `src/content/blog/` directory.
 	loader: glob({ base: './src/content/blog', pattern: '**/*.{md,mdx}' }),
 	// Type-check frontmatter using a schema
-	schema: ({ image }) =>
+	schema: ({ image }: SchemaContext) =>
 		z.object({
 			title: z.string(),
 			description: z.string(),
