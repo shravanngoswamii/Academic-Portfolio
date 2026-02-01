0745473829009dd2fab3aedce2485f4d3d44a545
Shravan Goswami <shravanngoswamii@gmail.com>
2026-02-01 15:53:00 +0530

    Make it a Nietzsche portfolio and philosophy focused

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Albert Einstein';
-export const SITE_DESCRIPTION = 'The academic portfolio of Albert Einstein.';
+export const SITE_TITLE = 'Friedrich Nietzsche';
+export const SITE_DESCRIPTION = 'The academic portfolio of Friedrich Nietzsche.';
